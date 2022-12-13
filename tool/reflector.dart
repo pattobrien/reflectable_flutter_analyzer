1cc15256eb2ae1e5f1e69b8066f4836070c45d87
pattobrien <12474318+pattobrien@users.noreply.github.com>
2022-12-13 13:58:17 -0500

    debug: Added Provider, Directory types to reflectable.

--- tool/reflector.dart
+++ tool/reflector.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
 // import 'dart:ui';
+import 'package:riverpod/riverpod.dart';
 import 'package:flutter/material.dart';
 
 import 'package:reflectable_analyzer/reflectable_analyzer.dart';
 
 @GlobalQuantifyCapability(r'\.Color$', analyzerReflector)
+@GlobalQuantifyCapability(r'\.Provider$', analyzerReflector)
+@GlobalQuantifyCapability(r'\.Directory$', analyzerReflector)
 import 'package:reflectable/reflectable.dart' hide SourceLocation;
 
 import 'reflector.reflectable.dart';
@@ -14,6 +17,9 @@ void main() {
   // reflect Color type
   final colorType = Color;
   final typeChecker = TypeChecker.fromRuntime(colorType, analyzerReflector);
+
+  final providerType = Provider;
+  final directoryType = Directory;
   // reflect Color instance
   final color = Color(0xFFFFFF00);
   final instance = analyzerReflector.reflect(color);
