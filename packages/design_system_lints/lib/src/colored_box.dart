a77557c92b2c111b5306655a8ca5e8304e538115
pattobrien <12474318+pattobrien@users.noreply.github.com>
2022-12-14 14:39:46 -0500

    docs: cleaned up visitor and example, added comments.

--- packages/design_system_lints/lib/src/colored_box.dart
+++ packages/design_system_lints/lib/src/colored_box.dart
@@ -2,64 +2,23 @@
 
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
-import 'package:analyzer/dart/element/type.dart';
 import 'package:flutter/material.dart';
 
 import 'package:reflectable_analyzer/reflectable_analyzer.dart';
 
-// @GlobalQuantifyCapability(r'\.Color$', analyzerReflector)
-// import 'package:reflectable/reflectable.dart';
-
 class ColorVisitor extends GeneralizingAstVisitor<int> {
-  // @override
-  // int? visitNode(AstNode node) {
-  //   print('visitNode ${node.runtimeType}: ${node.beginToken}');
-  //   return super.visitNode(node);
-  // }
-
-  // @override
-  // int? visitConstructorName(ConstructorName node) {
-  //   node.staticElement?.returnType;
-  //   return super.visitConstructorName(node);
-  // }
-
   @override
   int? visitInstanceCreationExpression(InstanceCreationExpression node) {
-    final interfaceType = node.constructorName.staticElement?.returnType;
-    // final constructorElement = node.constructorName.staticElement;
-    if (!node.staticType.isType(Color, analyzerReflector)) {
-      print('not a Color instance: ${node.beginToken}');
-      return super.visitInstanceCreationExpression(node);
+    // the below line works for Color, since we're able to reflect the library namespace of Color
+    if (node.staticType.isType(Color, analyzerReflector)) {
+      print('AstNode staticType is Color: ${node.staticType.toString()}');
     }
 
+    // the below line fails, because we cant reflect the source uri of Container
     if (node.staticType.isType(Container, analyzerReflector)) {
-      final instance = Container(key: Key('123'));
-      final mirror = analyzerReflector.reflect(instance);
-      print('got a Container instance: ${node.beginToken}');
-      return super.visitInstanceCreationExpression(node);
+      print('AstNode staticType is Container: ${node.staticType.toString()}');
     }
 
-    print('got a Color instance: ${node.staticType.toString()}');
-    print('instanceType ${interfaceType?.name}');
-    final value = node.argumentList.arguments.first;
-    final intValue = int.parse(value.toSource()).toRadixString(16);
-    print(intValue);
-
-    // final valueVariable =
-    //     constructorElement?.parameters.firstWhere((p) => p.isPositional);
-    // final val = valueVariable.;
-    // final valueVariable = interfaceType?.accessors
-    //     .firstWhere((element) => element.name == 'value')
-    //     .variable;
-    // final constantVal = valueVariable?.computeConstantValue();
-
-    print('instanceType accessors ${interfaceType?.accessors}');
-    // print('constant value: $valueVariable');
-    // final container = Mirror<Container>.fromNode(node);
-
-    // if (container.child != null && container.color != null) {
-    //     rule.reportLint(node.constructorName);
-    // }
     return super.visitInstanceCreationExpression(node);
   }
 }
